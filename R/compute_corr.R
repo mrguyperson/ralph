a15efcca63f25a3e4da75b3de65264834818eee5
Ted Hermann <ted.w.hermann@gmail.com>
2022-04-11 14:54:37 +0200

    adding tests

--- R/compute_corr.R
+++ R/compute_corr.R
@@ -14,19 +14,39 @@
 #'
 #' @importFrom rlang .data
 #'
-compute_corr <- function(data, var1, var2){
+compute_corr <- function(data, var1, var2) {
+
+  var1_char <- rlang::as_label(rlang::ensym(var1))
+  var2_char <- rlang::as_label(rlang::ensym(var2))
+
+  # alert user if variable not in data set ----
+
+  if (!(var1_char %in% names(data))){
+    #stop(glue::glue("{var1_char} is not in the data set."))
+    usethis::ui_stop("{var1_char} is not in the data set.")
+  }
+
+  if (!(var2_char %in% names(data))){
+    #stop(glue::glue("{var2_char} is not in the data set."))
+    usethis::ui_stop("{var2_char} is not in the data set.")
+
+  }
 
   # compute correlation ----
-  stats::cor.test(
-    x = data %>% dplyr::pull({{var1}}),
-    y = data %>% dplyr::pull({{var2}})
-  ) %>%
+  results <-
+    stats::cor.test(
+      x = data %>% dplyr::pull({{ var1 }}),
+      y = data %>% dplyr::pull({{ var2 }})
+    ) %>%
     # tidy up results ----
-  broom::tidy() %>%
+    broom::tidy() %>%
     # retain and rename relevant bits ----
-  dplyr::select(
-    correlation = .data$estimate,
-    pval = .data$p.value
-  )
+    dplyr::select(
+      correlation = .data$estimate,
+      pval = .data$p.value
+    )
 
+  attr(results$correlation, "names") <- NULL
+
+  return(results)
 }
